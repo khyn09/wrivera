9f6bbce8f05a9b2c183ee7dc4f5b6fc96a05b86c
khyn09 <74668150+khyn09@users.noreply.github.com>
2023-09-06 21:04:09 +0800

    Add files via upload

--- script/typing.js
+++ script/typing.js
@@ -1,15 +1,11 @@
-window.addEventListener('DOMContentLoaded', function () {
-    const header = document.getElementById('typing-header');
-    
-    function enableAnimation() {
-        header.style.animation = 'typing 3s steps(20, end) 1s forwards'; // Enable the animation
-        setTimeout(disableAnimation, 4000); // Wait for the animation to complete and then disable it
-    }
-
-    function disableAnimation() {
-        header.style.animation = 'none'; // Disable the animation
-        setTimeout(enableAnimation, 100); // Enable the animation again after a short delay
-    }
-
-    enableAnimation(); // Start the animation initially
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var options = {
+      strings: ["Frontend Developer", "Web Designer"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true,
+    };
+  
+    var typed = new Typed("#typed-output", options);
+  });
+  
